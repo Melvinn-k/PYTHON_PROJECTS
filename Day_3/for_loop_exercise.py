5bb42d5c5d754fbba81b9264eb5c947debef0be1
Melvinn-k <kinyuamelvin25@gmail.com>
2026-01-13 23:36:17 +0300

    For loop exercise

---
+++ Day_3/for_loop_exercise.py
@@ -0,0 +1,28 @@
+#Print Numbers
+#Print numbers from 1 to 5 using a for loop.
+for number in range(1,6):
+    print(number)
+
+#2️⃣ Repeat Message
+#Print "Python is fun" 3 times using a for loop.
+sentence = "Python is fine"
+for i in range(3):
+    print(sentence)
+
+#3️⃣ Print Even Numbers
+#Print all even numbers between 1 and 10.
+for number in range(1,11):
+    if number % 2 == 0 :
+        print(number)
+        
+#4️⃣ Print Odd Numbers
+#Print all odd numbers between 1 and 15.
+for number in range(1,16):
+    if number % 2 != 0:
+        print(number)
+
+#5️⃣ Count Up
+#Print numbers from 5 to 10.
+for number in range(5,11):
+    print(number)
+
